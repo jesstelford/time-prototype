4502ec46ceed4e9c2bc576f17a80dfea4c53ee80
Jess Telford <jess@jes.st>
2012-12-09 21:56:35 +1100

    First class in lua! (a Player)

--- main.lua
+++ main.lua
@@ -1,3 +1,32 @@
+dofile("player.lua")
+
+-- Called once before anything
+function love.load()
+    hamsterImage = love.graphics.newImage("hamster.png")
+    speed = 100 
+    player = Player(hamsterImage)
+end
+
+-- Called once per game loop
+function love.update(dt)
+
+    local dist = (speed * dt)
+
+    if love.keyboard.isDown("right") then
+        player:moveX(dist)
+    elseif love.keyboard.isDown("left") then
+        player:moveX(-dist)
+    end 
+
+    if love.keyboard.isDown("up") then
+        player:moveY(-dist)
+    elseif love.keyboard.isDown("down") then
+        player:moveY(dist)
+    end 
+
+end
+
+-- Called once per game loop
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    player:draw()
 end
---
+++ player.lua
@@ -0,0 +1,27 @@
+Class = require "hump.class"
+
+-- Player class
+Player = Class {
+    function(self, image)
+        -- constructor
+        self.image = image
+    end
+}
+
+Player.pos = {
+    x = 100,
+    y = 100
+}
+
+function Player:moveX(dist)
+    self.pos.x = self.pos.x + dist;
+end
+
+function Player:moveY(dist)
+    self.pos.y = self.pos.y + dist;
+end
+
+function Player:draw()
+    love.graphics.draw(self.image, self.pos.x, self.pos.y)
+end
+-- END Player class

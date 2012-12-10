d57098098d6f03681ebdc1e54a74f8c6228aa46e
Jess Telford <jess@jes.st>
2012-12-10 19:53:12 +1100

    Added a 'game' state

---
+++ State/Game.lua
@@ -0,0 +1,32 @@
+-- 'static' style Game state
+State_Game = Gamestate.new()
+
+function State_Game:init()
+    -- Set a variable on the game state object
+    State_Game.speed = 100
+end
+
+function State_Game:enter(previous)
+end
+
+function State_Game:update(dt)
+
+    local dist = (State_Game.speed * dt)
+
+    if love.keyboard.isDown("right") then
+        player:moveX(dist)
+    elseif love.keyboard.isDown("left") then
+        player:moveX(-dist)
+    end 
+
+    if love.keyboard.isDown("up") then
+        player:moveY(-dist)
+    elseif love.keyboard.isDown("down") then
+        player:moveY(dist)
+    end 
+
+end
+
+function State_Game:draw()
+    player:draw()
+end
--- main.lua
+++ main.lua
@@ -1,33 +1,22 @@
 Class = require "hump.class"
+Gamestate = require "hump.gamestate"
+
 dofile("Player.lua")
+dofile("State/Game.lua")
 
 -- Called once before anything
 function love.load()
     hamsterImage = love.graphics.newImage("hamster.png")
-    speed = 100 
     player = Player(hamsterImage)
+    Gamestate.switch(State_Game)
 end
 
 -- Called once per game loop
 function love.update(dt)
-
-    local dist = (speed * dt)
-
-    if love.keyboard.isDown("right") then
-        player:moveX(dist)
-    elseif love.keyboard.isDown("left") then
-        player:moveX(-dist)
-    end 
-
-    if love.keyboard.isDown("up") then
-        player:moveY(-dist)
-    elseif love.keyboard.isDown("down") then
-        player:moveY(dist)
-    end 
-
+    Gamestate.update(dt)
 end
 
 -- Called once per game loop
 function love.draw()
-    player:draw()
+    Gamestate.draw()
 end
